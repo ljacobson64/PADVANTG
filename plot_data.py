939c555b3c214275c7feb8640f6fa3a5fe1cceaf
Lucas John Jacobson <ljacobson64@gmail.com>
2019-08-18 18:46:46 -0500

    Plot material map

--- plot_data.py
+++ plot_data.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from cycler import cycler
 
 from draw_geom import draw_geometry
+from mcnp_colors import mcnp_colors as MC
 
 def main():
     data = read_all()
@@ -43,70 +44,74 @@ def plot_all(data):
     # Plot spectra
     plot_spectra(data['source_spectrum'], data['response_spectrum'])
 
+    # Plot material map
+    plot_data = data['material_map'][hz, :, :]
+    title = 'Material Map'
+    fname = 'material_map.png'
+    plot_flux(plot_data, x_vals, y_vals, title, fname, 'mats',
+              mat_names=data['mat_names'])
+
     # Plot total source
-    flux = np.sum(data['source'][hz, :, :, :], 2)
+    plot_data = np.sum(data['source'][hz, :, :, :], 2)
     title = 'Source (Total)'
     fname = 'source_total.png'
-    plot_flux(flux, x_vals, y_vals, title, fname, 'lin')
+    plot_flux(plot_data, x_vals, y_vals, title, fname, 'lin')
 
     # Plot total response
-    flux = np.sum(data['response'][hz, :, :, :], 2)
+    plot_data = np.sum(data['response'][hz, :, :, :], 2)
     title = 'Response (Total)'
     fname = 'response_total.png'
-    plot_flux(flux, x_vals, y_vals, title, fname, 'lin')
+    plot_flux(plot_data, x_vals, y_vals, title, fname, 'lin')
 
     # Plot total forward flux
-    flux = np.sum(data['flux_fwd'][hz, :, :, :], 2)
+    plot_data = np.sum(data['flux_fwd'][hz, :, :, :], 2)
     title = 'Forward Flux (Total)'
     fname = 'flux_forward_total.png'
-    plot_flux(flux, x_vals, y_vals, title, fname, 'log')
+    plot_flux(plot_data, x_vals, y_vals, title, fname, 'log')
 
     # Plot total adjoint flux
-    flux = np.sum(data['flux_adj'][hz, :, :, :], 2)
+    plot_data = np.sum(data['flux_adj'][hz, :, :, :], 2)
     title = 'Adjoint Flux (Total)'
     fname = 'flux_adjoint_total.png'
-    plot_flux(flux, x_vals, y_vals, title, fname, 'log')
+    plot_flux(plot_data, x_vals, y_vals, title, fname, 'log')
 
     # Plot total contributon
-    flux = np.sum(data['contributon'][hz, :, :, :], 2)
+    plot_data = np.sum(data['contributon'][hz, :, :, :], 2)
     title = 'Contributon (Total)'
     fname = 'contributon_total.png'
-    plot_flux(flux, x_vals, y_vals, title, fname, 'log')
+    plot_flux(plot_data, x_vals, y_vals, title, fname, 'log')
 
     # Plots for the fast group (2) and thermal group (26)
     for gx in [2, 26]:
         gf = gx - g0
 
-        flux = data['flux_fwd'][hz, :, :, gf]
+        plot_data = data['flux_fwd'][hz, :, :, gf]
         title = 'Forward Flux (Group %u)' % (gx)
         fname = 'flux_forward_g%02u.png'  % (gx)
-        plot_flux(flux, x_vals, y_vals, title, fname, 'log')
+        plot_flux(plot_data, x_vals, y_vals, title, fname, 'log')
 
-        flux = data['flux_adj'][hz, :, :, gf]
+        plot_data = data['flux_adj'][hz, :, :, gf]
         title = 'Adjoint Flux (Group %u)' % (gx)
         fname = 'flux_adjoint_g%02u.png'  % (gx)
-        plot_flux(flux, x_vals, y_vals, title, fname, 'log')
+        plot_flux(plot_data, x_vals, y_vals, title, fname, 'log')
 
-        flux = data['contributon'][hz, :, :, gf]
+        plot_data = data['contributon'][hz, :, :, gf]
         title = 'Contributon (Group %u)' % (gx)
         fname = 'contributon_g%02u.png'  % (gx)
-        plot_flux(flux, x_vals, y_vals, title, fname, 'log')
+        plot_flux(plot_data, x_vals, y_vals, title, fname, 'log')
 
     # Plot dR for all materials
     for im in range(nm):
-        flux = data['dR'][im, hz, :, :]
+        plot_data = data['dR'][im, hz, :, :]
         title = r'$\delta R$ for Material %u (%s)' % (im, data['mat_names'][im])
         fname = 'dR_%02u.png' % (im)
-        plot_flux(flux, x_vals, y_vals, title, fname, 'logplusminus')
+        plot_flux(plot_data, x_vals, y_vals, title, fname, 'logplusminus')
 
-def plot_flux(result, x_vals, y_vals, title, fname, fmt):
+def plot_flux(result, x_vals, y_vals, title, fname, fmt, mat_names=None):
     plt.rcParams['font.size'       ] = 10.0
     plt.rcParams['font.family'     ] = 'serif'
     plt.rcParams['mathtext.default'] = 'regular'
     plt.rcParams['axes.prop_cycle' ] = cycler(color=['k'])
-
-    fig, ax = plt.subplots(1, 1)
-    fig.set_size_inches(8, 6)
 
     # Color scale
     if fmt == 'logplusminus':
@@ -143,11 +148,35 @@ def plot_flux(result, x_vals, y_vals, title, fname, fmt):
         vmax = np.max(result) * 1.1
         norm = colors.Normalize(vmin=vmin, vmax=vmax)
         cmap = 'Reds'
+    elif fmt == 'mats':
+        with open('com_1', 'r') as reader: lines = reader.readlines()
+        CL = [MC['white']]
+        for line in lines:
+            if not line.startswith('shade'): continue
+            tokens = line.split()
+            CL.append(MC[tokens[2]])
+        vmin = 0
+        vmax = len(CL)
+        norm = colors.Normalize(vmin=vmin, vmax=vmax)
+        cmap = colors.ListedColormap(CL)
+        ticks = np.linspace(0.5, vmax - 0.5, len(CL))
+        tick_labels = list(mat_names)
+
+    fig, ax = plt.subplots(1, 1)
+    if 'mats' in fmt:
+        fig.set_size_inches(10, 6)
+        plt.subplots_adjust(left=-0.025, right=0.75)
+    else:
+        fig.set_size_inches(8, 6)
+        plt.subplots_adjust(left=0.125, right=0.9)
 
     # Plot data
     im = ax.pcolormesh(x_vals, y_vals, result, norm=norm, cmap=cmap)
 
     # Formatting
+    ax.set_aspect('equal', 'box')
+    ax.set_xlim([x_vals[0], x_vals[-1]])
+    ax.set_ylim([y_vals[0], y_vals[-1]])
     ax.set_xticks(np.linspace(x_vals[0], x_vals[-1], 5))
     ax.set_yticks(np.linspace(y_vals[0], y_vals[-1], 5))
     ax.set_xlabel('x position [cm]')
@@ -159,6 +188,11 @@ def plot_flux(result, x_vals, y_vals, title, fname, fmt):
     if 'log' in fmt:
         cbar.set_ticks(ticks)
         cbar.ax.set_yticklabels(tick_labels)
+    elif 'mats' in fmt:
+        cbar.set_ticks(ticks)
+        cbar.ax.set_yticklabels(tick_labels)
+        cbar.ax.tick_params(length=0)
+        cbar.ax.invert_yaxis()
 
     # Draw geometry
     draw_geometry()
