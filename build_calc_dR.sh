19a155739ace4a00a3a5d0c03f3ba0efeb4a14f2
Lucas John Jacobson <ljacobson64@gmail.com>
2019-08-20 19:37:07 -0500

    Use CMake to build cpp code

---
+++ build_calc_dR.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+jobs=`grep -c processor /proc/cpuinfo`
+install_prefix=${PWD}
+cnpy_dir=$(readlink -f ../build/cnpy)
+
+cmake_string=
+cmake_string+=" -DCNPY_DIR=${cnpy_dir}"
+cmake_string+=" -DCMAKE_INSTALL_PREFIX=${install_prefix}"
+cmake_string+=" -DCMAKE_BUILD_TYPE=Release"
+
+rm -rf bld
+mkdir bld
+cd bld
+cmake .. ${cmake_string}
+make -j${jobs}
+make install
+cd ..
+rm -rf bld
