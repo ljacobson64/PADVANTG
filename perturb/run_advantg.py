a1ffb93963270e41e38708ef599043cc71622d54
Lucas John Jacobson <ljacobson64@gmail.com>
2021-09-24 11:11:10 -0500

    Run with 3.2.0

--- perturb/run_advantg.py
+++ perturb/run_advantg.py
@@ -4,11 +4,13 @@ import os
 import sys
 
 def run_advantg():
-    advantg_dir = '/home/lucas/PADVANTG/ADVANTG-3.0.3/lib/python2.7/site-packages'
-    exnihilo_dir = '/opt/software_native/exnihilo-5.4.0/python'
+    advantg_dir  = '/home/lucas/PADVANTG/ADVANTG-3.2.0'
+    exnihilo_dir = '/opt/software_native/exnihilo-5.4.0'
+    advantg_python_dir  = advantg_dir  + '/lib/python2.7/site-packages'
+    exnihilo_python_dir = exnihilo_dir + '/python'
     sys.path = [x for x in sys.path if 'python3' not in x]
-    sys.path.insert(0, advantg_dir)
-    os.environ['PYTHONPATH'] = exnihilo_dir
+    sys.path.insert(0, advantg_python_dir)
+    os.environ['PYTHONPATH'] = exnihilo_python_dir
     from advantg.__main__ import main as advantg_main
 
     #os.system('rm -rf adj_solution fwd_solution model output custom_output')
