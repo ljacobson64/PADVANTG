cbe97a05ea8545770854c77ae875ee271ca3f709
Lucas John Jacobson <ljacobson64@gmail.com>
2021-10-04 13:53:24 -0500

    Symlink mcnp.i and model/ directory from forward run to all adjoint runs

--- perturb/run_advantg_all.py
+++ perturb/run_advantg_all.py
@@ -13,26 +13,37 @@ def main():
 
     # Run ADVANTG in each directory
     for direc in direc_list:
-        os.system('rm -rf ' + direc)
+        #os.system('rm -rf ' + direc)
         os.system('mkdir -pv ' + direc)
         os.chdir(direc)
+
+        # Write ADVANTG input file
         if direc.startswith('forward'): vals = ['true', 'false', '']
         else:
             tally = int(direc.split('-')[1])
             vals = ['false', 'true', 'mcnp_tallies%s%u' % (' ' * 16, tally)]
         with open('advantg.inp', 'w') as w: w.write(template.format(*vals))
-        with open('../mcnp.i', 'r') as r: lines = r.readlines()
-        s = ''
-        for line in lines:
-            tokens = line.split()
-            if len(tokens) == 0:
+
+        if direc.startswith('forward'):
+            # Write MCNP input file
+            with open('../mcnp.i', 'r') as r: lines = r.readlines()
+            s = ''
+            for line in lines:
+                tokens = line.split()
+                if len(tokens) == 0:
+                    s += line
+                    continue
+                if tokens[0].lower() == 'read':
+                    with open('../%s' % (tokens[3]), 'r') as r: s += r.read()
+                    continue
                 s += line
-                continue
-            if tokens[0].lower() == 'read':
-                with open('../%s' % (tokens[3]), 'r') as r: s += r.read()
-                continue
-            s += line
-        with open('mcnp.i', 'w') as w: w.write(s)
+            with open('mcnp.i', 'w') as w: w.write(s)
+        else:
+            # Copy MCNP input and output from forward/ directory
+            os.system('ln -snfv ../forward/mcnp.i .')
+            os.system('ln -snfv ../forward/model  .')
+
+        # Run ADVANTG
         os.system('bash ../../run_advantg_once.sh')
         os.chdir('..')
 
